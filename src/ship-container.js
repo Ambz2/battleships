671c76b44994de4706698e60315ff0b1ce7b1019
Ambz2 <ameenahmed1998@gmail.com>
2022-02-22 14:08:06 +0000

    Made player ship selector appear in order of ship size.

--- src/ship-container.js
+++ src/ship-container.js
@@ -6,9 +6,9 @@ export default class ShipContainer {
     this.element.classList = 'ship-container'
     this.dragNDropShip1 = new DragNDropShip(2);
     this.dragNDropShip2 = new DragNDropShip(3);
-    this.dragNDropShip3 = new DragNDropShip(4);
-    this.dragNDropShip4 = new DragNDropShip(5);
-    this.dragNDropShip5 = new DragNDropShip(3);
+    this.dragNDropShip3 = new DragNDropShip(3);
+    this.dragNDropShip4 = new DragNDropShip(4);
+    this.dragNDropShip5 = new DragNDropShip(5);
 
     this.element.appendChild(this.dragNDropShip1.element);
     this.element.appendChild(this.dragNDropShip2.element);
